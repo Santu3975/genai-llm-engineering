1d3c1df1099819bd89ec8a4b1837bddd30184fb9
B Santhosh Kumar Reddy <61876727+Santu3975@users.noreply.github.com>
2025-12-24 15:50:01 +0530

    Create docx_extractor.py

---
+++ text_extraction/docx_extractor.py
@@ -0,0 +1,9 @@
+
+
+from docx import Document
+
+def extract_docx(file_path: str) -> str:
+    doc = Document(file_path)
+    return "\n".join(
+        p.text for p in doc.paragraphs if p.text.strip()
+    )
